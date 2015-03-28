4a9dec03354dff50abebf77dbaaeb4699d12c162
Joseph Schiavone <jcschiavone@gmail.com>
2015-03-28 18:03:51 -0400

    Added create button

--- schema/views.py
+++ schema/views.py
@@ -5,3 +5,6 @@ import schema
 def home(request):
 	top_ten = BoycottPetition.objects.all()[:10]
 	return render(request, 'home.html', {'top':top_ten})
+
+def create(request):
+	return render(request, 'create.html', {})
