349fd96647ca92f2a22284571e2b44e780f6f62e
Joseph Schiavone <jcschiavone@gmail.com>
2015-03-28 16:11:54 -0400

    Terrible looking card template

--- schema/views.py
+++ schema/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from schema.models import *
 import schema
 
 def home(request):
-    return render(request, 'schema/home.html', {})
+	top_ten = BoycottPetition.objects.all()[:10]
+	return render(request, 'schema/home.html', {'top':top_ten})
