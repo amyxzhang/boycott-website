49b20a9d048a9f61b89fb4f8a5dfe1bcc2f5d5fc
Joseph Schiavone <jcschiavone@gmail.com>
2015-03-29 12:18:35 -0400

    Fixed submission page

--- schema/views.py
+++ schema/views.py
@@ -11,24 +11,7 @@ def home(request):
     return render(request, 'home.html', {'top':top_ten})
 
 def create(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = PetitionForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            created_at = models.DateTimeField(default=datetime.datetime.utcnow())
-            created_by = models.ForeignKey(User)
-            # redirect to a new URL:
-            return HttpResponseRedirect('/') # TODO
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = PetitionForm()
-
-    return render(request, 'create.html', {'form': form})
+    return render(request, 'create.html', {})
 
 def petition(request, petition_id):
     
